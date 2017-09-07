d092f823eb60edb4a26657be79f20efec2eb26e6
bassail <Bastien21>
2017-09-07 11:21:22 +0200

    [##] extracting stringbuilder to avoid passing it in method parameter

--- romannumberskata/src/main/java/RomanConverter.java
+++ romannumberskata/src/main/java/RomanConverter.java
@@ -1,19 +1,18 @@
 class RomanConverter {
 
+    private StringBuilder resultStringBuilder = new StringBuilder();
     String toRoman(int arabicNumber) {
-        StringBuilder result = new StringBuilder();
-
         int rest = arabicNumber;
         for (RomanArabicValue romanValue : RomanArabicValue.values()) {
-            rest = buildStringResult(rest, romanValue.arabic, romanValue.getRoman(), result);
+            rest = buildStringResult(rest, romanValue.arabic, romanValue.getRoman());
         }
-        return result.toString();
+        return resultStringBuilder.toString();
     }
 
-    private int buildStringResult(int arabicNumber, int romanToValue, String romanString, StringBuilder strb) {
+    private int buildStringResult(int arabicNumber, int romanToValue, String romanString) {
         int result = arabicNumber;
         while (result >= romanToValue) {
-            strb.append(romanString);
+            resultStringBuilder.append(romanString);
             result -= romanToValue;
         }
         return result;
